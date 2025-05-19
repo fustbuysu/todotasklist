93c42bb61796204e9f3ed4f5deccfea7cb15d0ea
unknown <22lbaoffc@gmail.com>
2025-05-20 01:13:27 +0300

    backup

--- conectdb.py
+++ conectdb.py
@@ -5,18 +5,55 @@ db_user = 'postgres'
 db_pw = '1'
 db_host = 'localhost'
 
+def get_connection():
+    return psycopg2.connect(dbname=db_name, user=db_user, password=db_pw, host=db_host)
+
 def gettasks():
-    connect = psycopg2.connect(dbname=db_name, user=db_user, password=db_pw, host=db_host)
+    connect = get_connection()
     curs = connect.cursor()
-    curs.execute('SELECT id, task_name, is_active FROM tasklist')
+    curs.execute('SELECT id, task_name, is_active, due_date FROM tasklist ORDER BY id')
     tasks = curs.fetchall()
-    curs.close
-    connect.close
+    curs.close()
+    connect.close()
     return tasks
 
-def add_task(name, date):
-    connect = psycopg2.connect(dbname=db_name, user=db_user, password=db_pw, host=db_host)
+def get_task(task_id):
+    connect = get_connection()
     curs = connect.cursor()
-    curs.execute('INSERT INTO public."tasklist"(task_name, due_date) VALUES ( \'%s\', \'%s\') ;commit;' %(name,date))
-    curs.close
-    connect.close
+    curs.execute('SELECT id, task_name, is_active, due_date FROM tasklist WHERE id = %s', (task_id,))
+    task = curs.fetchone()
+    curs.close()
+    connect.close()
+    return task
+
+def add_task(name, date):
+    connect = get_connection()
+    curs = connect.cursor()
+    curs.execute('INSERT INTO tasklist (task_name, due_date) VALUES (%s, %s)', (name, date))
+    connect.commit()
+    curs.close()
+    connect.close()
+
+def delete_task(task_id):
+    connect = get_connection()
+    curs = connect.cursor()
+    curs.execute('DELETE FROM tasklist WHERE id = %s', (task_id,))
+    connect.commit()
+    curs.close()
+    connect.close()
+
+def toggle_task(task_id):
+    connect = get_connection()
+    curs = connect.cursor()
+    curs.execute('UPDATE tasklist SET is_active = NOT is_active WHERE id = %s', (task_id,))
+    connect.commit()
+    curs.close()
+    connect.close()
+
+def update_task(task_id, name, date):
+    connect = get_connection()
+    curs = connect.cursor()
+    curs.execute('UPDATE tasklist SET task_name = %s, due_date = %s WHERE id = %s', (name, date, task_id))
+    connect.commit()
+    curs.close()
+    connect.close()
--- main.py
+++ main.py
@@ -1,18 +1,40 @@
 from flask import Flask, render_template, request, redirect, url_for
-from conectdb import gettasks, add_task
-app = Flask(__name__)
+from conectdb import gettasks, add_task, delete_task, toggle_task, get_task, update_task
 
-# tasklist = [['walk dog', True], ['wash dishes', False]]
+app = Flask(__name__)
 
 @app.route('/')
 def home():
     tasklist = gettasks()
     return render_template('todolist.html', TaskList=tasklist)
 
-@app.route('/add', methods =['POST'])
+@app.route('/add', methods=['POST'])
 def add():
     taskname = request.form['TaskName']
     duedate = request.form['DueDate']
     add_task(taskname, duedate)
     return redirect(url_for('home'))
-app.run()
+
+@app.route('/delete/<int:task_id>')
+def delete(task_id):
+    delete_task(task_id)
+    return redirect(url_for('home'))
+
+@app.route('/toggle/<int:task_id>')
+def toggle(task_id):
+    toggle_task(task_id)
+    return redirect(url_for('home'))
+
+@app.route('/edit/<int:task_id>', methods=['GET', 'POST'])
+def edit(task_id):
+    if request.method == 'POST':
+        new_name = request.form['TaskName']
+        new_date = request.form['DueDate']
+        update_task(task_id, new_name, new_date)
+        return redirect(url_for('home'))
+    else:
+        task = get_task(task_id)
+        return render_template('edit_task.html', task=task)
+
+if __name__ == '__main__':
+    app.run(debug=True)
